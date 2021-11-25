62e7ff8bed11331201a98a5d6fa8e2f23487e75e
Nguyen Nhat <nhatnmph18030@fpt.edu.vn>
2021-11-25 22:22:19 +0700

    Hoàn thành luồng trả hàng trong hóa đơn bán hàng của Tuấn

--- QLBanHangQuanAo/src/giaoDien/PFrmHoaDonChiTiet_trangThai_soLuong.java
+++ QLBanHangQuanAo/src/giaoDien/PFrmHoaDonChiTiet_trangThai_soLuong.java
@@ -1,18 +1,24 @@
-
 package giaoDien;
 
 import helper.XJdbc;
-import java.sql.PreparedStatement;
-import javax.swing.JOptionPane;
 
 public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
 
-    public PFrmHoaDonChiTiet_trangThai_soLuong(String tenSP, int MaHD, int idSP) {
+    int soLuong;
+    int trangThai;
+    double donGia;
+
+    public PFrmHoaDonChiTiet_trangThai_soLuong(String tenSP, int MaHD, int idSP, int soLuong, double donGia, int trangThai) {
         initComponents();
+        this.soLuong = soLuong;
+        this.trangThai = trangThai;
+        this.donGia = donGia;
         lbltenSP.setText(tenSP);
         setLocationRelativeTo(null);
         lblMaHD.setText(MaHD + "");
         lblIDsp.setText(idSP + "");
+        txtSoLuong.setText(soLuong + "");
+        cboTrangThai.setSelectedIndex(trangThai);
     }
 
     @SuppressWarnings("unchecked")
@@ -41,17 +47,17 @@ public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
 
         jLabel2.setText("Trạng thái hoá đơn :");
 
-        lbltenSP.setText("jLabel1");
+        lbltenSP.setText("Tên SP");
 
-        cboTrangThai.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Huỷ", "Thanh Toán" }));
+        cboTrangThai.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Thanh Toán", "Trả hàng" }));
 
-        jLabel1.setText("Mã hoá đơn");
+        jLabel1.setText("Mã hoá đơn:");
 
-        lblMaHD.setText("jLabel1");
+        lblMaHD.setText("Mã HĐ");
 
-        jLabel4.setText("Mã sản phẩm chi tiết");
+        jLabel4.setText("Mã sản phẩm chi tiết:");
 
-        lblIDsp.setText("jLabel1");
+        lblIDsp.setText("Mã SPCT");
 
         jLabel3.setText("Số lượng :");
 
@@ -65,22 +71,29 @@ public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
                         .addGap(151, 151, 151)
                         .addComponent(lbltenSP))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(37, 37, 37)
+                        .addGap(50, 50, 50)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel3))
-                        .addGap(43, 43, 43)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lblMaHD)
-                            .addComponent(cboTrangThai, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(lblIDsp)
-                            .addComponent(txtSoLuong)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(159, 159, 159)
-                        .addComponent(btnTrangThai)))
-                .addContainerGap(104, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addGap(43, 43, 43)
+                                .addComponent(lblIDsp))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(92, 92, 92)
+                                .addComponent(txtSoLuong, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(85, 85, 85)
+                                .addComponent(lblMaHD))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(43, 43, 43)
+                                .addComponent(cboTrangThai, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
+                .addContainerGap(91, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(157, 157, 157)
+                .addComponent(btnTrangThai)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,7 +108,7 @@ public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(lblIDsp))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(txtSoLuong, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -103,46 +116,60 @@ public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(cboTrangThai, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
+                .addGap(37, 37, 37)
                 .addComponent(btnTrangThai)
-                .addGap(57, 57, 57))
+                .addContainerGap(44, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnTrangThaiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTrangThaiActionPerformed
-        try {
-            int MaHD = Integer.valueOf(lblMaHD.getText());
-            int idsp = Integer.valueOf(lblIDsp.getText());
-            int soLuong = Integer.valueOf(txtSoLuong.getText());
-            if (soLuong > 0) {
-                String sql2 = "update HoaDonChiTiet set SoLuong = ? where IDHoaDon = ? and IDCTSP = ?";
-                PreparedStatement pstm2 = XJdbc.getStmt(sql2);
-                pstm2.setInt(1, soLuong);
-                pstm2.setInt(2, MaHD);
-                pstm2.setInt(3, idsp);
-                int row2 = pstm2.executeUpdate();
-
-            } else {
-                String sql3 = "DELETE FROM HoaDonChiTiet WHERE IDHoaDon = ? and IDCTSP = ?";
-                PreparedStatement pstm3 = XJdbc.getStmt(sql3);
-                pstm3.setInt(1, MaHD);
-                pstm3.setInt(2, idsp);
-                int row3 = pstm3.executeUpdate();
-            }
-            String sql = "update HoaDonChiTiet set TrangThai = ? where IDHoaDon = ? and IDCTSP = ?";
-            PreparedStatement pstm2 = XJdbc.getStmt(sql);
-            int trangThai = cboTrangThai.getSelectedIndex();
-            pstm2.setInt(1, trangThai);
-            pstm2.setInt(2, MaHD);
-            pstm2.setInt(3, idsp);
-            int row2 = pstm2.executeUpdate();
-            if (row2 > 0) {
-                JOptionPane.showMessageDialog(this, "update trang thái thành công với trạng thái : " + trangThai);
-            }
-        } catch (Exception e) {
+        if (validateNumber()) {
+            return;
         }
+        int maHD = Integer.valueOf(lblMaHD.getText());
+        int idsp = Integer.valueOf(lblIDsp.getText());
+        int soLuongMoi = Integer.valueOf(txtSoLuong.getText());
+        int tt = cboTrangThai.getSelectedIndex();
+        int soLuongThua = soLuong - soLuongMoi;
+
+        if (tt == 0) {
+            try {
+                String sql = "update HoaDonChiTiet set SoLuong = ?, TrangThai = ? where IDHoaDon = ? and IDCTSP = ?";
+                int row = XJdbc.update(sql, soLuongMoi, tt, maHD, idsp);
+
+                String sql2 = "insert into HoaDonChiTiet(IDHoaDon, IDCTSP, DonGia, SoLuong, TrangThai)\n"
+                        + "values(?, ?, ?, ?, ?)";
+                int row2 = XJdbc.update(sql2, maHD, idsp, donGia, soLuongThua, 1);
+                
+                String sql3 = "update ChiTietSanPham set Soluong = Soluong + ? where IDCTSP = ?";
+                int row3 = XJdbc.update(sql3, soLuongThua, idsp);
+                if (row2 > 0 && row > 0 && row3 > 0) {
+                    helper.MsgBox.alert(null, "Thành công");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (tt == 1){
+            try {
+                String sql = "update HoaDonChiTiet set SoLuong = ?, TrangThai = ? where IDHoaDon = ? and IDCTSP = ?";
+                int row = XJdbc.update(sql, soLuongMoi, tt, maHD, idsp);
+
+                String sql2 = "insert into HoaDonChiTiet(IDHoaDon, IDCTSP, DonGia, SoLuong, TrangThai)\n"
+                        + "values(?, ?, ?, ?, ?)";
+                int row2 = XJdbc.update(sql2, maHD, idsp, donGia, soLuongThua, 0);
+                
+                String sql3 = "update ChiTietSanPham set Soluong = Soluong + ? where IDCTSP = ?";
+                int row3 = XJdbc.update(sql3, soLuongMoi, idsp);
+                if (row > 0 && row2 > 0 && row3 > 0) {
+                    helper.MsgBox.alert(null, "Thành công");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }//GEN-LAST:event_btnTrangThaiActionPerformed
 
 //    public static void main(String args[]) {
@@ -189,5 +216,27 @@ public class PFrmHoaDonChiTiet_trangThai_soLuong extends javax.swing.JFrame {
     private javax.swing.JLabel lbltenSP;
     private javax.swing.JTextField txtSoLuong;
     // End of variables declaration//GEN-END:variables
+
+    private boolean validateNumber() {
+        int amount;
+        if (txtSoLuong.getText().isEmpty()) {
+            helper.MsgBox.alert(null, "Xin mời nhập số lượng");
+            txtSoLuong.requestFocus();
+            return true;
+        }
+        try {
+            amount = Integer.valueOf(txtSoLuong.getText());
+        } catch (Exception e) {
+            helper.MsgBox.alert(null, "Xin mời nhập số lượng là số");
+            txtSoLuong.requestFocus();
+            return true;
+        }
+        if (amount <= 0 || amount > this.soLuong) {
+            helper.MsgBox.alert(null, "Xin mời nhập số lượng lớn hơn 0 và nhỏ hơn số lượng trong đơn hàng");
+            txtSoLuong.requestFocus();
+            return true;
+        }
+        return false;
+    }
 
 }
